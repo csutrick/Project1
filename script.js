ee37dee4a71fa88a9d0c61ff3f98de5ea810b746
jimh <james.l.heinecke@gmail.com>
2023-04-17 19:46:31 -0500

    omdb function updates

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 async function OMDbData() {
-    const response = await fetch("http://www.omdbapi.com/?" + new URLSearchParams({apikey:'93572ae7', s: 'avatar' }));
-    response.searchParams.set()
+    var searchPar = document.querySelector('#srch').value;
+    console.log(search)
+    var key = '93572ae7';
+    //type = movie,series,episode
+    //s = search parameter
+    //y = year of release
+
+    const response = await fetch("http://www.omdbapi.com/?" + 'apikey='+key+'&s='+searchPar);
+    // response.searchParams.set()
     const jsonData = await response.json();
-    console.log(jsonData);
+    console.log(jsonData)
+    var results = jsonData.Search[0].Title;
+    var results1 = jsonData.Search[1].Title;
+    var results2 = jsonData.Search[2].Title;
+    var results3 = jsonData.Search[3].Title;
+    var results4 = jsonData.Search[4].Title;
+    var results5 = jsonData.Search[5].Title;
+    
+    document.getElementById("result").innerHTML = results;
+    document.getElementById("result1").innerHTML = results1;
+    document.getElementById("result2").innerHTML = results2;
+    document.getElementById("result3").innerHTML = results3;
+    document.getElementById("result4").innerHTML = results4;
+    document.getElementById("result5").innerHTML = results5;
 }
